813a239b0c10ee8e07da2de2b6fdedb51a64b8ae
Mir Carvalho <deppbrazil@gmail.com>
2019-05-22 03:01:03 -0300

    bugFix menu dashboard

---
+++ assets/js/pages/dashboard/dashboard.js
@@ -0,0 +1,125 @@
+function showWMS() {
+
+    var click = document.getElementById('drop__content--WMS');
+    var arrowWMS = document.getElementById('arrow--WMS');
+
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+
+    if (arrowWMS.style.transform == "rotate(90deg)") {
+        arrowWMS.style.transform = "rotate(0deg)";
+    } else {
+        arrowWMS.style.transform = "rotate(90deg)";
+    }
+}
+
+function showTMS() {
+
+    var click = document.getElementById('drop__content--TMS');
+    var arrowWMS = document.getElementById('arrow--TMS');
+
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+
+    if (arrowWMS.style.transform == "rotate(90deg)") {
+        arrowWMS.style.transform = "rotate(0deg)";
+    } else {
+        arrowWMS.style.transform = "rotate(90deg)";
+    }
+}
+
+function showBroker() {
+
+    var click = document.getElementById('drop__content--broker');
+    var arrowWMS = document.getElementById('arrow--broker');
+
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+
+    if (arrowWMS.style.transform == "rotate(90deg)") {
+        arrowWMS.style.transform = "rotate(0deg)";
+    } else {
+        arrowWMS.style.transform = "rotate(90deg)";
+    }
+}
+
+function showCustomer() {
+
+    var click = document.getElementById('drop__content--customer');
+    var arrowWMS = document.getElementById('arrow--customer');
+
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+
+    if (arrowWMS.style.transform == "rotate(90deg)") {
+        arrowWMS.style.transform = "rotate(0deg)";
+    } else {
+        arrowWMS.style.transform = "rotate(90deg)";
+    }
+}
+
+function showERP() {
+
+    var click = document.getElementById('drop__content--erp');
+    var arrowWMS = document.getElementById('arrow--erp');
+
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+
+    if (arrowWMS.style.transform == "rotate(90deg)") {
+        arrowWMS.style.transform = "rotate(0deg)";
+    } else {
+        arrowWMS.style.transform = "rotate(90deg)";
+    }
+}
+
+function showSupply() {
+
+    var click = document.getElementById('drop__content--supply');
+    var arrowWMS = document.getElementById('arrow--supply');
+
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+
+    if (arrowWMS.style.transform == "rotate(90deg)") {
+        arrowWMS.style.transform = "rotate(0deg)";
+    } else {
+        arrowWMS.style.transform = "rotate(90deg)";
+    }
+}
+
+function showSolutions() {
+
+    var click = document.getElementById('drop__content--solutions');
+    var arrowWMS = document.getElementById('arrow--solutions');
+
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+
+    if (arrowWMS.style.transform == "rotate(90deg)") {
+        arrowWMS.style.transform = "rotate(0deg)";
+    } else {
+        arrowWMS.style.transform = "rotate(90deg)";
+    }
+}
--- assets/js/pages/dashboard/dropdown.js
+++
@@ -1,9 +0,0 @@
-function show() {
-    var click = document.getElementById('drop-content');
-
-    if (click.style.display === "none") {
-        click.style.display = "flex";
-    } else {
-        click.style.display = "none";
-    }
-}
