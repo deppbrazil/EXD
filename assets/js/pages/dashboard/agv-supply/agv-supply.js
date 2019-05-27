2c30da1416acabb6cf046e792547474cf0ebb907
Mir Carvalho <deppbrazil@gmail.com>
2019-05-27 16:40:36 -0300

    upgrade (Busca Avançada)

--- assets/js/pages/dashboard/agv-supply/agv-supply.js
+++ assets/js/pages/dashboard/agv-supply/agv-supply.js
@@ -1,10 +1,28 @@
 function showBuscaRapida() {
 
     var click = document.getElementById('busca__rapida');
+    var clickFecha = document.getElementById('busca__avancada');
     
     if (click.style.display === "none") {
         click.style.display = "flex";
+        clickFecha.style.display = "none";
     } else {
         click.style.display = "none";
     }
+}
+
+function showBuscaAvancada() {
+
+    document.getElementById('busca__rapida').style.diplay = "none";
+
+    var click = document.getElementById('busca__avancada');
+    var clickFecha = document.getElementById('busca__rapida');
+    
+    if (click.style.display === "none") {
+        click.style.display = "block";
+        clickFecha.style.display = "none";
+    } else {
+        click.style.display = "none";
+    }
+
 }
