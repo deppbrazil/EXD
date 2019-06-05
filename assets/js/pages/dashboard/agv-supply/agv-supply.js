efa7f425106b1a0583a1679873a6118ee0c56673
Mir Carvalho <deppbrazil@gmail.com>
2019-06-05 18:23:39 -0300

    upgrade (Busca Rápida - Lista da Busca Rápida e Detalhes)

--- assets/js/pages/dashboard/agv-supply/agv-supply.js
+++ assets/js/pages/dashboard/agv-supply/agv-supply.js
@@ -1,3 +1,4 @@
+// Go Busca Rápida //
 function btnBuscaRapida() {
 
     var click = document.getElementById('busca__rapida');
@@ -9,6 +10,54 @@ function btnBuscaRapida() {
     clickClose2.style.display = "none";
 }
 
+// Go List - Busca Rápida //
+function goListBucaRapida() {
+    var clickHide = document.getElementById('homeBuscaRapida');
+    var clickShow = document.getElementById('busca__rapida__detalhes');
+
+    clickHide.style.display = "none";
+    clickShow.style.display = "flex";
+}
+
+// Go - Busca Rápida - Detalhes //
+function btnGoBuscaRapida() {
+
+    var clickHide = document.getElementById('detalhes');
+    var clickShow = document.getElementById('busca__rapida__detalhes');
+
+    clickHide.style.display = "none";
+    clickShow.style.display = "flex";
+}
+
+// Detalhes da Nota //
+function btnGoDetalhes() {
+
+    var clickHide = document.getElementById('busca__rapida__detalhes');
+    var clickShow = document.getElementById('detalhes');
+
+    clickHide.style.display = "none";
+    clickShow.style.display = "block";
+}
+
+// Back Home - Busca Rápida //
+function btnGoHomeBuscaRapida() {
+
+    var clickHide = document.getElementById('busca__rapida__detalhes');
+    var clickShow = document.getElementById('homeBuscaRapida');
+
+    clickHide.style.display = "none";
+    clickShow.style.display = "flex";
+}
+
+
+
+
+
+
+
+
+
+// Go - Busca Avançada //
 function btnBuscaAvancada() {
     
     var click = document.getElementById('busca__avancada');
@@ -17,10 +66,10 @@ function btnBuscaAvancada() {
 
     click.style.display = "block";
     clickClose1.style.display = "none";
-    clickClose2.style.display = "none";
-    
+    clickClose2.style.display = "none"; 
 }
 
+// Go Dashboard //
 function btnDashboard() {
     
     var click = document.getElementById('dashboard');
